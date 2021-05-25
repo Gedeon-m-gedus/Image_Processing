af5f90b4a17637b2b217544c0d94be3eb5948561
Gedeon Muhawenayo <50500636+Gedeon-m-gedus@users.noreply.github.com>
2021-05-25 20:27:28 +0200

    converting video to images

---
+++ video2images.py
@@ -0,0 +1,33 @@
+import cv2 
+import os 
+def createImagesFromVideo(VideoPath):
+    # Read the video from specified path 
+    cam = cv2.VideoCapture(VideoPath) 
+    try: 
+        # creating a folder named data 
+        if not os.path.exists('data'): 
+            os.makedirs('data') 
+    # if not created then raise error 
+    except OSError: 
+        print ('Error: Creating directory of data') 
+    # frame 
+    currentframe = 0
+    while(True): 
+        # reading from frame 
+        ret,frame = cam.read() 
+        if ret: 
+            # if video is still left continue creating images 
+            name = './data/frame' + str(currentframe) + '.jpg'
+            print ('Creating...' + name) 
+
+            # writing the extracted images 
+            cv2.imwrite(name, frame) 
+
+            # increasing counter so that it will 
+            # show how many frames are created 
+            currentframe += 1
+        else: 
+            break
+    # Release all space and windows once done 
+    cam.release() 
+    cv2.destroyAllWindows() 
